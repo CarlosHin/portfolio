4cef8db9802682a50dbf223f0db2530baaa8434a
Carlos <carloshin98@gmail.com>
2023-01-12 19:37:19 +0100

    install partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,21 +2,16 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import partytown from "@astrojs/partytown";
-
 import astroI18next from "astro-i18next";
 
+// https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    react(),
-    astroI18next(),
-    partytown({
-      // Adds dataLayer.push as a forwarding-event.
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
-  ]
+  integrations: [tailwind(), react(), astroI18next(), partytown({
+    // Adds dataLayer.push as a forwarding-event.
+    config: {
+      forward: ["dataLayer.push"]
+    }
+  })]
 });
